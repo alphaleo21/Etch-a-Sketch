2c5447710456358738fce0dc574493d239e25ade
alphin <alphinpoly8@gmail.com>
2023-05-31 15:48:42 +0530

    container element created

--- jss.js
+++ jss.js
@@ -1,2 +1,11 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('#container');
 
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+
+for(let i = 0;i<25; i++){
+    const grid = document.createElement('div');
+    grid.classList.add('grid')
+    grid.style.backgroundColor = "black";
+    container.insertAdjacentElement("beforeend", grid);
+}
