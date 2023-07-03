e044d196ebad10d773f0b1a6a1bd37ce72751d46
alphin <alphinpoly8@gmail.com>
2023-07-03 20:00:49 +0530

    grids created along with change color on hover

--- jss.js
+++ jss.js
@@ -1,12 +1,44 @@
 const container = document.querySelector('#container');
+let count = 0;
 
-for(let j =0;j<16;j++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let i=0;i<16;i++){
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        row.appendChild(grid);
+function gridCreation(e){
+    if(count<1){
+        for(let j =0;j<e;j++){
+            const row = document.createElement('div');
+            row.classList.add('row');
+            for(let i=0;i<e;i++){
+                const grid = document.createElement('div');
+                grid.classList.add('grid');
+                grid.addEventListener("mouseover", () =>{
+                    grid.style.backgroundColor = changeColor();
+                })
+                row.appendChild(grid);
+            }
+            container.appendChild(row);
+        }
+        count++;
+    }else if(count == 1){
+        const row = document.querySelectorAll(".row")
+        const grid = document.querySelectorAll(".grid")
+        row.clear;
+        grid.clear;
+        count = 0;
+        console.log("hello")
     }
-    container.appendChild(row);
+    
+}
+
+const color = document.getElementById('color');
+const bgColor = document.getElementById('bgcolor');
+
+function clear(){
+    
+}
+
+function changeColor(){
+    return color.value
+}
+
+function changeSize(input){
+    gridCreation(input)
 }
